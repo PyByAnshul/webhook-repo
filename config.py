c304c07898d2fb57e5c0a851b037a6736b38b010
PyByAnshul <a9756549615@gmail.com>
2025-07-05 13:39:39 +0530

    [update] mongo uri

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class BaseConfig:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'git_webhook_secret_key'
-    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://localhost:27017/git_webhook_db'
+    MONGO_URI = "mongodb+srv://a9756549615:dvPXhoiKNnSQ2q21@webhook.bjswhy4.mongodb.net/webhook-mongo?retryWrites=true&w=majority"
     WEBHOOK_TIMEOUT = 30  
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # Max payload size: 16 MB
     UI_UPDATE_INTERVAL = 15  # seconds
